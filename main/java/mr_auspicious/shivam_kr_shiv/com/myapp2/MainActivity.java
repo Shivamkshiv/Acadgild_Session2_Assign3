76db0783b5421714d25e1745b8ad7f3c71d72b76
Shivam Kumar <shivamhclme5@gmail.com>
2017-01-01 16:46:33 +0530

    Add files via upload

---
+++ main/java/mr_auspicious/shivam_kr_shiv/com/myapp2/MainActivity.java
@@ -0,0 +1,13 @@
+package mr_auspicious.shivam_kr_shiv.com.myapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
